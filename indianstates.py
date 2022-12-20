2c7535364647987cb85ba98162a31b7e51eb75a5
imran8583 <114766622+imran8583@users.noreply.github.com>
2022-12-20 13:11:47 +0530

    upload indianstates.py

---
+++ indianstates.py
@@ -0,0 +1,24 @@
+# Create a dictionary of any 7 Indian states and their capitals. Write this into a JSON file.
+
+import json
+
+indian_states = {
+    "Telangana": "Hyderabad",
+    "Karnataka": "Banglore",
+    "Maharastra": "Mumbai",
+    "West Bengal": "Kolkata",
+    "Tamil Nadu": "Chennai",
+    "Rajasthan": "Jaipur",
+    "Uttarakhand": "Dehradun",
+}
+
+# Create a new json file in write mode to add above data into it.
+with open(r"indian_states.json", "w") as file:
+
+    # Write the dictionary to the file in JSON format
+    json.dump(indian_states, file)
+
+
+with open(r"indian_states.json", "r") as file:
+    data = file.readlines()
+    print(data)
