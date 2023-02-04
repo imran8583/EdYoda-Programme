8768828662a3d2698ce841a51f3e8a565524aaab
imran8583 <114766622+imran8583@users.noreply.github.com>
2023-02-04 23:34:56 +0530

    Add files via upload

--- Technical Coding Assessment Problem 1.sql
+++ Technical Coding Assessment Problem 1.sql
@@ -4,19 +4,3 @@ country_name  varchar(40),
 region_id  decimal(10,0),
 CHECK(COUNTRY_NAME IN('Italy','India','China'))
 );
-
-INSERT INTO countries (country_id, country_name, region_id)
-VALUES (1, 'India', 111);
-
-INSERT INTO countries (country_id, country_name, region_id)
-VALUES (2, 'China', 222);
-
-INSERT INTO countries (country_id, country_name, region_id)
-VALUES (3, 'USA', 333);
-
-INSERT INTO countries (country_id, country_name, region_id)
-VALUES (3, 'Italy', 333);
-
-SELECT * FROM countries;
-
-DESC countries;
